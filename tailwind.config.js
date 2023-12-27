ebdf67fbafb807e96ae042bb6af6ac6f91a3e4f5
Atyla A. Al Harits <damsadam78@gmail.com>
2023-12-27 21:59:55 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        "cream-resume": "#F8F4EB"
+      },
+      fontFamily: {
+        "monserrat": "Monserrat, sans-serif",
+        "noto-sans": "Noto Sans, sans-serif",
+        "poppins": "Poppins, sans-serif"
+      }
+    },
+  },
+  plugins: [],
+}
+
